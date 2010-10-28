eed00b33319fc5ef2ecb332733cf89b6b90b5d25
Philip Guo <philip@pgbovine.net>
2010-10-28 14:43:22 -0700

    improved bootstrap test slightly

--- tests/CDE-bootstrap/testme.py
+++ tests/CDE-bootstrap/testme.py
@@ -12,7 +12,7 @@ time.sleep(0.3) # to give os.system some time to work :)
 
 Popen(["make", "clean"], stdout=PIPE, stderr=PIPE).communicate()
 
-(stdout, stderr) = run_cde(["make"], True)
+(first_run_stdout, first_run_stderr) = run_cde(["make"], True)
 
 generic_lib_checks()
 
@@ -21,6 +21,22 @@ assert os.path.isfile('cde-root/usr/bin/gcc')
 
 Popen(["make", "clean"], stdout=PIPE, stderr=PIPE).communicate()
 
+
+# to make for a tougher test, move the entire directory to /tmp
+# and try to do a cde-exec run
+full_pwd = os.getcwd()
+cur_dirname = os.path.basename(full_pwd)
+tmp_test_dir = "/tmp/" + cur_dirname
+
+# careful with these commands!
+(stdout, stderr) = Popen(["rm", "-rf", tmp_test_dir], stdout=PIPE, stderr=PIPE).communicate()
+assert not stdout and not stderr
+(stdout, stderr) = Popen(["cp", "-aR", full_pwd, "/tmp"], stdout=PIPE, stderr=PIPE).communicate()
+assert not stdout and not stderr
+
+# run the cde-exec test in tmp_test_dir
+os.chdir(tmp_test_dir)
+
 (stdout2, stderr2) = Popen([CDE_EXEC_SPECIAL, "make"], stdout=PIPE, stderr=PIPE).communicate()
 
 #print "=== stdout:"
@@ -28,5 +44,8 @@ Popen(["make", "clean"], stdout=PIPE, stderr=PIPE).communicate()
 #print "=== stdout2:"
 #print stdout2
 
-#assert stdout == stdout2
-assert stderr == stderr2
+#assert first_run_stdout == stdout2
+assert first_run_stderr == stderr2
+
+os.chdir(full_pwd) # make sure to chdir back!!!
+
